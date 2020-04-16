d9114820d5b096efd9ed30121c8cdef0ce10ea94
dosbenjamin <dosbenjamin@gmail.com>
2020-04-16 16:01:30 +0200

    :art: Modify socket file structure

--- src/js/utils/socketConfig.js
+++ src/js/utils/socketConfig.js
@@ -1,28 +1,7 @@
 import io from 'socket.io-client'
 
-// TODO: Détecter si l'utilisateur à accès raspberry.
-
-const rpi = {
-  url: 'wss://clever-owl-36.telebit.io',
-  isConnected: false,
-  socket () {
-    return io.connect(this.url, {
-      autoConnect: false,
-      transports: ['websocket']
-    })
-  },
-  connect () {
-    return this.socket().connect()
-  },
-  disconnect () {
-    return this.socket().disconnect()
-  }
-}
-
 const middleware = io.connect('wss://rpi.benjamindossantos.be', { transports: ['polling'] })
-middleware.on('connected', state => {
-  state ? rpi.connect() : rpi.disconnect()
-  rpi.isConnected = state
-})
+export const rpi = io.connect('wss://clever-owl-36.telebit.io', { autoConnect: false, transports: ['websocket'] })
 
-// export default socket
+middleware.on('state_update', state => state && rpi.connect())
+rpi.on('disconnect', () => rpi.disconnect())
