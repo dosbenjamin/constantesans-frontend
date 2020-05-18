a0dd4a627a6a628fd6008dc16cd347dc9c9cb64e
dosbenjamin <dosbenjamin@gmail.com>
2020-05-18 21:14:53 +0200

    🎨 Update light experience with static sensor

--- src/js/components/experiences/products/LightExperience.js
+++ src/js/components/experiences/products/LightExperience.js
@@ -4,49 +4,57 @@ import Experience from '../Experience'
  * Class representing a light experience.
  */
 export default class LightExperience extends Experience {
-  /**
-   * Initializes and manages a light experience.
-   */
-  constructor () {
-    super('light')
-
-    this.state = 'off'
-    this.values = {
-      content: { on: 'allumée', off: 'éteinte' },
-      weight: { on: 100, off: 900 }
-    }
-  }
+  constructor () { super('light') }
 
   /**
-   * Starts the experience when the modal is opened.
+   * Start the experience when the modal is opened.
    *
    * @returns {void} Nothing
    */
   start () {
-    this.sensor.emit('light_check')
-    this.sensor.on('light_update', this.setVariation)
+    Experience.sensors.emit('check_light')
+    Experience.sensors.on('light_update', state => this.setVariation(state))
   }
 
   /**
-   * Starts the experience when the modal is opened.
+   * Stop the experience when the modal is closed.
    *
    * @returns {void} Nothing
    */
   stop () {
     this.$values[0].textContent = ''
     this.$values[1].textContent = ''
-    this.sensor.off('light_update')
+    Experience.sensors.off('light_update')
   }
 
   /**
-   * Applies typography style and inserts values inside modal on light changes.
+   * Apply typography style and insert values inside modal on light changes.
    *
-   * @param {boolean} state - The state of the light (on/off).
+   * @param {string} state - The state of the light (on/off).
    * @returns {void} Nothing
    */
   setVariation (state) {
-    this.state = state
-    this.$values[0].textContent = this.values.content[this.state]
-    this.$values[1].textContent = this.values.weight[this.state]
+    const variations = {
+      content: { on: 'allumée', off: 'éteinte' },
+      weight: { on: 200, off: 900 },
+      style: {
+        /**
+         * Remove the dark appareance when the light is on.
+         *
+         * @returns {void} Nothing
+         */
+        on: () => this.$experience.classList.remove('c-experience--dark'),
+
+        /**
+         * Add the dark appareance when the light is off.
+         *
+         * @returns {void} Nothing
+         */
+        off: () => this.$experience.classList.add('c-experience--dark')
+      }
+    }
+    this.$values[0].textContent = variations.content[state]
+    this.$values[1].textContent = variations.weight[state]
+    variations.style[state]()
   }
 }
