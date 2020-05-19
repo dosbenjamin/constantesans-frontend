86f8cc1c2974674dd34e03dbaa809d4170a88609
dosbenjamin <dosbenjamin@gmail.com>
2020-05-19 16:09:47 +0200

    💫 Add animation to mouve/device moves

---
+++ src/js/components/moveVariations.js
@@ -0,0 +1,27 @@
+import { getValueInRange } from '../helpers/range'
+
+/**
+ * The animation when the mouse/device is moving.
+ *
+ * @param {object} event - The event
+ * @returns {void} Nothing
+ */
+const move = event => {
+  const coords = (({ x, y }) => ({ x, y }))(event)
+  const sizes = {
+    height: getValueInRange(coords.y / window.innerHeight * 100, 100, 900),
+    width: getValueInRange(coords.x / window.innerWidth * 100, 50, 100)
+  }
+  document.body.style['font-variation-settings'] = `'wght' ${sizes.height}, 'wdth' ${sizes.width}`
+}
+
+export default {
+  /**
+   * Start to animate typography when the mouse/device is moving.
+   *
+   * @returns {void} Nothing
+   */
+  init () {
+    window.addEventListener('mousemove', move)
+  }
+}
